0ba2a55bfe756119e1d04cca4ace029d7a7dd82b
Nikita Ksenofontov <ms.ksenofontov@mail.ru>
2020-06-14 03:47:31 +0300

    Урок 2 Задание 6

--- Урок 2. Практическое задание/task_6/task_6_1.py
+++ Урок 2. Практическое задание/task_6/task_6_1.py
@@ -7,3 +7,28 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+
+from random import randint
+
+print('Отгадайете чилсло от 0 до 100, у вас 10 попыток')
+number = randint(0, 100)
+count = 1
+while count <= 10:
+    print(f'Текущая попытка: {count}')
+    try:
+        answer = int(input('Введите предположительное число: '))
+        if answer < 0:
+            print('Введено отрицательное число')
+            continue
+        if answer == number:
+            print('Поздравляем, вы угадали')
+            break
+        elif answer < number:
+            print('Число больше, чем вы указали')
+        else:
+            print('Число меньше чем вы указали')
+        count += 1
+    except ValueError:
+        print('Некорректный ввод')
+else:
+    print('Вы проиграли, не угадав число')
